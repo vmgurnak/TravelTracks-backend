2036ad8ba778bc2aa38be0fb5bd996ef0385da02
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-10-11 12:23:28 +0300

    connected to MongoDB

--- src/server.js
+++ src/server.js
@@ -9,16 +9,15 @@ const PORT = env('PORT', 3000);
 const startServer = () => {
   const app = express();
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  });
 
-  app.use(express.json());
+  app.use(logger);
   app.use(cors());
+  app.use(express.json());
 
   app.get('/campers', (req, res) => {
     res.json({ message: 'Response from server route /campers' });
@@ -29,13 +28,13 @@ const startServer = () => {
     res.json({ message: `Response from server route /campers/${id}` });
   });
 
-  app.use('*', (req, res) => {
+  app.use('*', (req, res, next) => {
     res.status(404).json({ message: 'Route not found' });
   });
 
   app.use((err, req, res, next) => {
-    console.error(err);
-    res.send(err);
+    const { status = 500, message = 'Server error' } = err;
+    res.status(status).json({ message });
   });
 
   app.listen(PORT, () => {
