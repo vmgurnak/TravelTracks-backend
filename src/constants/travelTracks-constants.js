b4f84209ad4950b89c1fad7a67e815bb4838def1
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-12-23 11:32:09 +0200

    updated

---
+++ src/constants/travelTracks-constants.js
@@ -0,0 +1,7 @@
+export const TravelTracksForm = ['panelTruck', 'fullyIntegrated', 'alcove'];
+
+export const TravelTracksRating = [1, 2, 3, 4, 5];
+
+export const TravelTracksTransmission = ['manual', 'automatic'];
+
+export const TravelTracksEngine = ['diesel', 'petrol', 'hybrid', 'electric'];
--- src/db/models/TravelTracks.js
+++ src/db/models/TravelTracks.js
@@ -1,30 +1,104 @@
 import { Schema, model } from 'mongoose';
 
+import {
+  TravelTracksForm,
+  TravelTracksTransmission,
+  TravelTracksEngine,
+} from '../../constants/travelTracks-constants.js';
+
 const travelTracksSchema = new Schema(
   {
-    name: String,
-    price: Number,
-    rating: Number,
-    location: String,
-    description: String,
-    form: String,
-    length: String,
-    width: String,
-    height: String,
-    tank: String,
-    consumption: String,
-    transmission: String,
-    engine: String,
-    AC: Boolean,
-    bathroom: Boolean,
-    kitchen: Boolean,
-    TV: Boolean,
-    radio: Boolean,
-    refrigerator: Boolean,
-    microwave: Boolean,
-    gas: Boolean,
-
-    water: Boolean,
+    name: {
+      type: String,
+      required: [true, 'name is required'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'price is required'],
+    },
+    rating: {
+      type: Number,
+      required: [true, 'rating is required'],
+    },
+    location: {
+      type: String,
+      required: [true, 'location is required'],
+    },
+    description: {
+      type: String,
+      required: [true, 'description is required'],
+    },
+    form: {
+      type: String,
+      enum: TravelTracksForm,
+      default: 'panelTruck',
+    },
+    length: {
+      type: String,
+      required: [true, 'length is required'],
+    },
+    width: {
+      type: String,
+      required: [true, 'width is required'],
+    },
+    height: {
+      type: String,
+      required: [true, 'height is required'],
+    },
+    tank: {
+      type: String,
+      required: [true, 'tank is required'],
+    },
+    consumption: {
+      type: String,
+      required: [true, 'consumption is required'],
+    },
+    transmission: {
+      type: String,
+      enum: TravelTracksTransmission,
+      required: [true, 'transmission is required'],
+    },
+    engine: {
+      type: String,
+      enum: TravelTracksEngine,
+      required: [true, 'engine is required'],
+    },
+    AC: {
+      type: Boolean,
+      required: [true, 'AC is required'],
+    },
+    bathroom: {
+      type: Boolean,
+      required: [true, 'bathroom is required'],
+    },
+    kitchen: {
+      type: Boolean,
+      required: [true, 'kitchen is required'],
+    },
+    TV: {
+      type: Boolean,
+      required: [true, 'TV is required'],
+    },
+    radio: {
+      type: Boolean,
+      required: [true, 'radio is required'],
+    },
+    refrigerator: {
+      type: Boolean,
+      required: [true, 'refrigerator is required'],
+    },
+    microwave: {
+      type: Boolean,
+      required: [true, 'microwave is required'],
+    },
+    gas: {
+      type: Boolean,
+      required: [true, 'gas is required'],
+    },
+    water: {
+      type: Boolean,
+      required: [true, 'water is required'],
+    },
     gallery: [
       {
         thumb: String,
@@ -54,5 +128,9 @@ const travelTracksSchema = new Schema(
   },
   { versionKey: false, timestamps: true },
 );
+
+// travelTracksSchema.post('save', mongooseSaveError);
+// travelTracksSchema.pre('findOneAndUpdate', setUpdateSettings);
+// travelTracksSchema.post('findOneAndUpdate', mongooseSaveError);
 
 export default model('traveltracks', travelTracksSchema);
