99baead54f4507cc1a54e933acfc1d3394e4c5a5
Valeriy.Gurnak <vmgurnak@gmail.com>
2025-02-07 12:25:16 +0200

    validation shemas

---
+++ src/validation/camper-shemas.js
@@ -0,0 +1,134 @@
+import Joi from 'joi';
+
+import {
+  TravelTracksForm,
+  TravelTracksTransmission,
+  TravelTracksEngine,
+} from '../constants/travelTracks-constants.js';
+
+export const camperAddShema = Joi.object({
+  name: Joi.string().required().messages({
+    'any.required': 'name is required',
+  }),
+  price: Joi.number().required().messages({
+    'any.required': 'price is required',
+  }),
+  rating: Joi.number().min(1).max(5).required().messages({
+    'any.required': 'rating is required',
+  }),
+  location: Joi.string().required().messages({
+    'any.required': 'location is required',
+  }),
+  description: Joi.string().required().messages({
+    'any.required': 'description is required',
+  }),
+  form: Joi.string()
+    .valid(...TravelTracksForm)
+    .required()
+    .messages({ 'any.required': 'form is required' }),
+  length: Joi.string().required().messages({
+    'any.required': 'length is required',
+  }),
+  width: Joi.string().required().messages({
+    'any.required': 'width is required',
+  }),
+  height: Joi.string().required().messages({
+    'any.required': 'height is required',
+  }),
+  tank: Joi.string().required().messages({
+    'any.required': 'tank is required',
+  }),
+  consumption: Joi.string().required().messages({
+    'any.required': 'consumption is required',
+  }),
+  transmission: Joi.string()
+    .valid(...TravelTracksTransmission)
+    .required()
+    .messages({ 'any.required': 'transmission is required' }),
+  engine: Joi.string()
+    .valid(...TravelTracksEngine)
+    .required()
+    .messages({
+      'any.required': 'engine is required',
+    }),
+  AC: Joi.boolean().required().messages({
+    'any.required': 'AC is required',
+  }),
+  bathroom: Joi.boolean().required().messages({
+    'any.required': 'bathroom is required',
+  }),
+  kitchen: Joi.boolean().required().messages({
+    'any.required': 'kitchen is required',
+  }),
+  TV: Joi.boolean().required().messages({
+    'any.required': 'TV is required',
+  }),
+  radio: Joi.boolean().required().messages({
+    'any.required': 'radio is required',
+  }),
+  refrigerator: Joi.boolean().required().messages({
+    'any.required': 'refrigerator is required',
+  }),
+  microwave: Joi.boolean().required().messages({
+    'any.required': 'microwave is required',
+  }),
+  gas: Joi.boolean().required().messages({
+    'any.required': 'gas is required',
+  }),
+  water: Joi.boolean().required().messages({
+    'any.required': 'water is required',
+  }),
+  gallery: Joi.array().items(
+    Joi.object({
+      thumb: Joi.string().required().messages({
+        'any.required': 'thumb is required',
+      }),
+      original: Joi.string(),
+    }),
+  ),
+  reviews: Joi.array().items(
+    Joi.object({
+      reviewer_name: Joi.string(),
+      reviewer_rating: Joi.number().min(1).max(5),
+      comment: Joi.string(),
+    }),
+  ),
+});
+
+export const camperUpdateShema = Joi.object({
+  name: Joi.string(),
+  price: Joi.number(),
+  rating: Joi.number().min(1).max(5),
+  location: Joi.string(),
+  description: Joi.string(),
+  form: Joi.string().valid(...TravelTracksForm),
+  length: Joi.string(),
+  width: Joi.string(),
+  height: Joi.string(),
+  tank: Joi.string(),
+  consumption: Joi.string(),
+  transmission: Joi.string().valid(...TravelTracksTransmission),
+  engine: Joi.string().valid(...TravelTracksEngine),
+  AC: Joi.boolean(),
+  bathroom: Joi.boolean(),
+  kitchen: Joi.boolean(),
+  TV: Joi.boolean(),
+  radio: Joi.boolean(),
+  refrigerator: Joi.boolean(),
+  microwave: Joi.boolean(),
+  gas: Joi.boolean(),
+  water: Joi.boolean(),
+  gallery: Joi.array().items(
+    Joi.object({
+      thumb: Joi.string(),
+      original: Joi.string(),
+    }),
+  ),
+  reviews: Joi.array().items(
+    Joi.object({
+      reviewer_name: Joi.string(),
+      reviewer_rating: Joi.number().min(1).max(5),
+      comment: Joi.string(),
+    }),
+  ),
+});
